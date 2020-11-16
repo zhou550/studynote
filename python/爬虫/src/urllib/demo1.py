c20cb3fa5f082ca4ff997533c73448c06c3f56a7
zhou hao <14324380@qq.com>
2020-11-16 23:06:00 +0800

    11-16 Python爬虫学习之request模块

---
+++ python/爬虫/src/urllib/demo1.py
@@ -0,0 +1,9 @@
+from urllib import request
+
+# resp = request.urlopen("http://www.baidu.com")
+# print(resp.read())
+
+
+# request.urlretrieve("http://www.baidu.com",'baidu.html')
+
+request.urlretrieve("https://puui.qpic.cn/media_img/0/729581541579559/0",'tanyan.png')
